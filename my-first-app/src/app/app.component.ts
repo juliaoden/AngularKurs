2a1aa41f4b255ea8d24d182cb43b7d70a4739b78
juliaoden <julia_oden@web.de>
2023-02-20 19:57:08 +0100

    first commit

---
+++ my-first-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  /*styles: [
+    `
+      h3 {
+        color: #7d40a1;
+      }
+    `,
+  ],*/
+})
+export class AppComponent {}
